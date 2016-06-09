817f3366749ac540cbca5f1b34fc25f9d1a461b3
Rebecca Yang <rebeccayang123@gmail.com>
2016-06-08 21:25:31 -0700

    Treetop grammar for evaluating key value pairs
    Sets up sonar treetop
    
    Modifies treetop grammar
    
    Expands on treetop grammar to handle AND/OR
    
    Fixes bug

---
+++ sonar_grammar.rb
@@ -0,0 +1,8 @@
+# module Grammar
+#   include Treetop::Runtime
+#   class KeyValuePaired < Treetop::Runtime::SyntaxNode
+#     # def eval
+#     #   # case query.text_value
+#     # end
+#   end
+# end
