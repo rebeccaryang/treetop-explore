796921ca42b527eff93316eb8f26362f0ab59354
Rebecca Yang <rebeccayang123@gmail.com>
2016-06-13 16:50:37 -0700

    Adds date parsing

--- sonar_treetop.rb
+++ sonar_treetop.rb
@@ -1,18 +1,19 @@
 require 'rubygems'
+require 'pry'
 require 'pry-stack_explorer'
 require 'treetop'
-# require_relative 'sonar_grammar'
+require_relative 'sonar_grammar'
 Treetop.load 'sonar_grammar'
 
 parser = SonarGrammarParser.new
 test_cases = [
-    ['friend:true AND dolphin:not', true],
+    ['OR friend:true dolphin:not', true],
     ['7=7', false],
     ['6 = 7', false],
     ['godzilla', false]
 ]
 
 test_cases.each do |test_case|
-  puts "#{test_case[0]} is #{parser.parse(test_case[0])}, " \
+  puts "#{test_case[0]} is #{parser.parse(test_case[0]).eval}, " \
        "expected #{test_case[1]}"
 end
